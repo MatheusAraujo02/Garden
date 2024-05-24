aa2776f6fb7a8d247e7dbee697022f2ac0c9ed9c
MATEUS APARECIDO PESSOA RODR <mateus.rodrigues64@etectupa.local>
2024-05-24 20:07:50 -0300

    Initial commit

---
+++ Garden/src/index.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import { ImageBackground, Pressable, StyleSheet,  Text, TextInput, View} from 'react-native';
+
+import fundologin from '../assets/fundologin.png';
+
+import styles from '../src/styles';
+
+
+export default function Login ( ) {
+    return (
+        <View style={styles.container} >
+
+            <ImageBackground
+             source={fundologin} style={styles.container}
+            >
+            <Text style={styles.titulo}>GARDEN</Text>
+
+            <Text style={styles.text}>Usuário</Text>
+            <TextInput style={styles.input}></TextInput>
+
+            <Text style={styles.text}>Senha</Text>
+            <TextInput style={styles.input}></TextInput>
+
+            <View style={styles.button}>
+            <Pressable >
+                <Text style={styles.txtButton}>
+                    ENTRAR
+                </Text>
+            </Pressable>
+            </View>
+
+            <Text style={styles.textPeq}>Esqueceu a senha?</Text>
+            <Text style={styles.textPeq}>Cadastre-se</Text>
+            </ImageBackground>
+        </View>
+
+    );
+}
---
+++ Garden/src/styles.js
@@ -0,0 +1,71 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container:{
+         width: '100%', 
+         height: '100%', 
+        // alignItems: 'center', 
+        backgroundColor: '#236c1a', 
+        padding: 8,
+        borderRadius: 10, 
+        
+    },
+
+    titulo:{ 
+        alignItems: 'center', 
+        textAlign: 'center',
+        padding: 8,
+        color: '#0F3820',
+    },
+
+    text:{
+        fontSize: 15,
+        color: '#fff',
+        display: 'flex',
+        alignContent: 'stretch',
+    },
+
+    input:{
+        fontSize: 10, 
+        width: '100%', 
+        textAlign: 'center', 
+        borderWidth: 1, 
+        borderColor: '#808080',  
+        padding: 5,
+        
+    },
+
+    button: {
+        color: '#fff',
+        backgroundColor: '#074A05',
+        margin: 5,
+        padding: 10,
+        width: '25%',
+        borderRadius: 10,
+        display: 'flex',
+        alignItems: 'center'
+    },
+    txtButton: {
+        color: '#fff',
+        fontSize: 10,
+        flex: 1,
+        alignContent: 'center',
+        alignItems: 'center',
+        
+
+    },
+
+    backg: {
+        width: '100%',
+        height: '100%',
+        zIndex: 1,
+    },
+
+    textPeq: {
+        color: '#fff',
+        fontSize: 6,
+        textAlign: 'center'
+    },
+});
+
+export default styles;
